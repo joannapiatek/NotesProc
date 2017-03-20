8197e095a669f7cc7fcb96ea4bee504399f5c194
Joanna Piątek <Joanna Pitek>
2017-03-20 23:02:56 +0100

    Staff lines detection - RLE vertical algorithm in test file

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import sys
+from itertools import *
+# from matplotlib import pyplot as plt
 
-img = cv2.imread('oko.jpg',0)
 
-##cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-##cv2.waitKey(0)
-##cv2.imshow('image',img)
-##cv2.waitKey(0)
-##cv2.destroyAllWindows()
+def encode(x):
+    return [(name, len(list(group))) for name, group in groupby(x)]
 
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+
+img = cv2.imread('img/test.png', 0)
+img = zip(*img)
+imgSize = np.shape(img)
+
+encoded = [imgSize[0]]
+i = 0
+
+for line in img:
+    x = encode(img[i])
+    encoded.append(x)
+    i += 1
+
+sys.exit(0)
+
+
