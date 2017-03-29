720e65c4ce0fc0db0118a141434bb8a2d136afc2
Joanna Piątek <Joanna Pitek>
2017-03-29 23:46:31 +0200

    Finding shortest path is working properly. Test for black percent needed.

--- test_graph.py
+++ test_graph.py
@@ -8,12 +8,12 @@ import Constants.Colors as colors
 # import scipy.ndimage as ndimage
 
 
-img = cv2.imread('img/test.png', 0)
+img = cv2.imread('img/test1.png', 0)
 imgSize = np.shape(img)
 
-# graph = nx.Graph()
-graph = igraph.Graph()
-graph.add_vertex(10)
+graph = nx.Graph()
+# graph = igraph.Graph()
+
 # graph.add_edge(0, 1, weight = 2)
 
 
@@ -21,11 +21,16 @@ end = imgSize[0]
 endCol = imgSize[1]
 # for row in range(0, end):
 
-for row in range(0, end):
-    for col in range(0, endCol):
-        name = str(row) + '_' + str(col)
-        graph.add_vertex(name)
+# vertices = []
+# for row in range(0, end):
+#     for col in range(0, endCol):
+#         name = str(row) + '_' + str(col)
+#         vertices.append(name)
+#
+# graph.add_vertices(vertices)
 
+edges_for_graph = []
+weights = []
 row = 0
 for row in range(0, end):
     for col in range(0, endCol):
@@ -34,17 +39,24 @@ for row in range(0, end):
         edges = gh.get_edges(gm.Pixel(row, col, color), nbhs)
         for edge in edges:
             graph.add_edge(edge.start, edge.end, weight=edge.weight)
+            # edges_for_graph.append((edge.start, edge.end))
+            # weights.append(edge.weight)
+
+# graph.add_edges(edges_for_graph)
+# graph.es["weight"] = weights
 
 
+x = graph.node.keys()
 staffLines = []
-xd = graph.node.keys()
 
 for row in range(0, end):
-    # if colors.BLACK not in img[row]:
-    #     continue
+    if colors.BLACK not in img[row]:
+        continue
 
-    startNode = (row, 0)
-    endNode = (row, end)
+    startNode = str(row) + '_' + str(0)
+    endNode = str(row) + '_' + str(end)
+    # startNode = (row, 0)
+    # endNode = (row, end)
     result = nx.bidirectional_dijkstra(graph, startNode, endNode)
     staffLines.append(result)
 
